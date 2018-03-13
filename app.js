c5a0b73fcad54879c046061f8f0782b71d4cc171
Mauro <mauro@jurincic.net>
2018-03-13 20:56:58 +0100

    refactored code

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ const expressSession = require('express-session');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const passport = require('passport');
-const util = require('util');
 const bunyan = require('bunyan');
 const config = require('./config');
+const address = require('address');
 
 // set up database for express session
 const MongoStore = require('connect-mongo')(expressSession);
@@ -46,7 +46,7 @@ const mongoose = require('mongoose');
 const OIDCStrategy = require('passport-azure-ad').OIDCStrategy;
 
 const log = bunyan.createLogger({
-    name: 'Microsoft OIDC Example Web Application'
+  name: 'Microsoft OIDC Example Web Application'
 });
 
 /******************************************************************************
@@ -64,7 +64,7 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(oid, done) {
-  findByOid(oid, function (err, user) {
+  findByOid(oid, function(err, user) {
     done(err, user);
   });
 });
@@ -75,7 +75,7 @@ const users = [];
 const findByOid = function(oid, fn) {
   for (let i = 0, len = users.length; i < len; i++) {
     const user = users[i];
-   log.info('we are using user: ', user);
+    log.info('we are using user: ', user);
     if (user.oid === oid) {
       return fn(null, user);
     }
@@ -100,47 +100,49 @@ const findByOid = function(oid, fn) {
 //
 // To do prototype (6), passReqToCallback must be set to true in the config.
 //-----------------------------------------------------------------------------
-passport.use(new OIDCStrategy({
-    identityMetadata: config.creds.identityMetadata,
-    clientID: config.creds.clientID,
-    responseType: config.creds.responseType,
-    responseMode: config.creds.responseMode,
-    redirectUrl: config.creds.redirectUrl,
-    allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
-    clientSecret: config.creds.clientSecret,
-    validateIssuer: config.creds.validateIssuer,
-    isB2C: config.creds.isB2C,
-    issuer: config.creds.issuer,
-    passReqToCallback: config.creds.passReqToCallback,
-    scope: config.creds.scope,
-    loggingLevel: config.creds.loggingLevel,
-    nonceLifetime: config.creds.nonceLifetime,
-    nonceMaxAmount: config.creds.nonceMaxAmount,
-    useCookieInsteadOfSession: config.creds.useCookieInsteadOfSession,
-    cookieEncryptionKeys: config.creds.cookieEncryptionKeys,
-    clockSkew: config.creds.clockSkew,
-  },
-  function(iss, sub, profile, accessToken, refreshToken, done) {
-    if (!profile.oid) {
-      return done(new Error("No oid found"), null);
-    }
-    // asynchronous verification, for effect...
-    process.nextTick(function () {
-      findByOid(profile.oid, function(err, user) {
-        if (err) {
-          return done(err);
-        }
-        if (!user) {
-          // "Auto-registration"
-          users.push(profile);
-          return done(null, profile);
-        }
-        return done(null, user);
+passport.use(
+  new OIDCStrategy(
+    {
+      identityMetadata: config.creds.identityMetadata,
+      clientID: config.creds.clientID,
+      responseType: config.creds.responseType,
+      responseMode: config.creds.responseMode,
+      redirectUrl: config.creds.redirectUrl,
+      allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
+      clientSecret: config.creds.clientSecret,
+      validateIssuer: config.creds.validateIssuer,
+      isB2C: config.creds.isB2C,
+      issuer: config.creds.issuer,
+      passReqToCallback: config.creds.passReqToCallback,
+      scope: config.creds.scope,
+      loggingLevel: config.creds.loggingLevel,
+      nonceLifetime: config.creds.nonceLifetime,
+      nonceMaxAmount: config.creds.nonceMaxAmount,
+      useCookieInsteadOfSession: config.creds.useCookieInsteadOfSession,
+      cookieEncryptionKeys: config.creds.cookieEncryptionKeys,
+      clockSkew: config.creds.clockSkew
+    },
+    function(iss, sub, profile, accessToken, refreshToken, done) {
+      if (!profile.oid) {
+        return done(new Error('No oid found'), null);
+      }
+      // asynchronous verification, for effect...
+      process.nextTick(function() {
+        findByOid(profile.oid, function(err, user) {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            // "Auto-registration"
+            users.push(profile);
+            return done(null, profile);
+          }
+          return done(null, user);
+        });
       });
-    });
-  }
-));
-
+    }
+  )
+);
 
 //-----------------------------------------------------------------------------
 // Config the app, include middlewares
@@ -149,32 +151,38 @@ const app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.logger());
 app.use(methodOverride());
 app.use(cookieParser());
 
 // set up session middleware
 if (config.useMongoDBSessionStore) {
   mongoose.connect(config.databaseUri);
-  app.use(express.session({
-    secret: 'secret',
-    cookie: {maxAge: config.mongoDBSessionMaxAge * 1000},
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      clear_interval: config.mongoDBSessionMaxAge
+  app.use(
+    express.session({
+      secret: 'secret',
+      cookie: { maxAge: config.mongoDBSessionMaxAge * 1000 },
+      store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        clear_interval: config.mongoDBSessionMaxAge
+      })
     })
-  }));
+  );
 } else {
-  app.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: false }));
+  app.use(
+    expressSession({
+      secret: 'keyboard cat',
+      resave: true,
+      saveUninitialized: false
+    })
+  );
 }
 
-app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(app.router);
 app.use(express.static(__dirname + '/../../public'));
 
 //-----------------------------------------------------------------------------
@@ -189,9 +197,11 @@ app.use(express.static(__dirname + '/../../public'));
 // it will call `passport.authenticate` to ask for user to log in.
 //-----------------------------------------------------------------------------
 function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
+  if (req.isAuthenticated()) {
+    return next();
+  }
   res.redirect('/login');
-};
+}
 
 app.get('/', function(req, res) {
   res.render('index', { user: req.user });
@@ -202,65 +212,82 @@ app.get('/account', ensureAuthenticated, function(req, res) {
   res.render('account', { user: req.user });
 });
 
-app.get('/login',
+app.get(
+  '/login',
   function(req, res, next) {
-    passport.authenticate('azuread-openidconnect',
-      {
-        response: res,                      // required
-        resourceURL: config.resourceURL,    // optional. Provide a value if you want to specify the resource.
-        customState: 'my_state',            // optional. Provide a value if you want to provide custom state value.
-        failureRedirect: '/'
-      }
-    )(req, res, next);
+    passport.authenticate('azuread-openidconnect', {
+      response: res, // required
+      resourceURL: config.resourceURL, // optional. Provide a value if you want to specify the resource.
+      customState: 'my_state', // optional. Provide a value if you want to provide custom state value.
+      failureRedirect: '/'
+    })(req, res, next);
   },
   function(req, res) {
     log.info('Login was called in the Sample');
     res.redirect('/');
-});
+  }
+);
 
 // 'GET returnURL'
 // `passport.authenticate` will try to authenticate the content returned in
 // query (such as authorization code). If authentication fails, user will be
 // redirected to '/' (home page); otherwise, it passes to the next middleware.
-app.get('/auth/openid/return',
+app.get(
+  '/auth/openid/return',
   function(req, res, next) {
-    passport.authenticate('azuread-openidconnect',
-      {
-        response: res,                      // required
-        failureRedirect: '/'
-      }
-    )(req, res, next);
+    passport.authenticate('azuread-openidconnect', {
+      response: res, // required
+      failureRedirect: '/'
+    })(req, res, next);
   },
   function(req, res) {
     log.info('We received a return from AzureAD.');
     res.redirect('/');
-  });
+  }
+);
 
 // 'POST returnURL'
 // `passport.authenticate` will try to authenticate the content returned in
 // body (such as authorization code). If authentication fails, user will be
 // redirected to '/' (home page); otherwise, it passes to the next middleware.
-app.post('/auth/openid/return',
+app.post(
+  '/auth/openid/return',
   function(req, res, next) {
-    passport.authenticate('azuread-openidconnect',
-      {
-        response: res,                      // required
-        failureRedirect: '/'
-      }
-    )(req, res, next);
+    passport.authenticate('azuread-openidconnect', {
+      response: res, // required
+      failureRedirect: '/'
+    })(req, res, next);
   },
   function(req, res) {
     log.info('We received a return from AzureAD.');
     res.redirect('/');
-  });
+  }
+);
 
 // 'logout' route, logout from passport, and destroy the session with AAD.
-app.get('/logout', function(req, res){
+app.get('/logout', function(req, res) {
+  // eslint-disable-next-line no-unused-vars
   req.session.destroy(function(err) {
     req.logOut();
     res.redirect(config.destroySessionUrl);
   });
 });
 
+const { HOST = '0.0.0.0' } = process.env;
 const { PORT = 3000 } = process.env;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); // eslint-disable-line no-console
+let localUrlForTerminal = `http://${HOST}:${PORT}/`;
+let lanUrlForTerminal = `http://${HOST}:${PORT}/`;
+if (HOST === '0.0.0.0' || HOST === '::') {
+  localUrlForTerminal = `http://localhost:${PORT}/`;
+  lanUrlForTerminal = `http://${address.ip()}:${PORT}/`;
+}
+app.listen(PORT, () =>
+  // eslint-disable-next-line no-console
+  console.log(`
+Access URLs:
+--------------------------------------
+Localhost: ${localUrlForTerminal}
+      LAN: ${lanUrlForTerminal}
+--------------------------------------
+Press CTRL-C to stop`)
+);
--- config.js
+++ config.js
@@ -1,9 +1,45 @@
-require('dotenv-extended').load({
-    errorOnMissing: true,
-    errorOnMissing: true
-});
+const joi = require('joi');
+require('dotenv').config();
 
-exports.creds = {
+// Check for required env variables and their types.
+const envVarsSchema = joi
+  .object({
+    NODE_ENV: joi
+      .string()
+      .valid(['development', 'production', 'test', 'provision'])
+      .required(),
+    PORT: joi.number().required(),
+    LOGGER_LEVEL: joi
+      .string()
+      .valid(['error', 'warn', 'info', 'verbose', 'debug', 'silly'])
+      .default('info'),
+    LOGGER_ENABLED: joi
+      .boolean()
+      .truthy('TRUE')
+      .truthy('true')
+      .falsy('FALSE')
+      .falsy('false')
+      .default(true),
+    AZURE_AD_TENANT_GUID: joi.string().guid(),
+    AZURE_AD_CLIENT_ID: joi.string().guid(),
+    AZURE_AD_CLIENT_SECRET: joi.string(),
+    DATABASE_MONGO_URI: joi.string().uri({
+      scheme: ['mongodb']
+    })
+  })
+  .required();
+
+const { error, value: envVars } = joi.validate(process.env, envVarsSchema, {
+  abortEarly: false,
+  convert: true,
+  allowUnknown: true
+});
+if (error) {
+  throw new Error(`Environment variables validation error: ${error.message}`);
+}
+
+module.exports = {
+  creds: {
     // Required
     // 'https://login.microsoftonline.com/<tenant_name>.onmicrosoft.com/.well-known/openid-configuration'
     // or equivalently: 'https://login.microsoftonline.com/<tenant_guid>/.well-known/openid-configuration'
@@ -11,7 +47,9 @@ exports.creds = {
     // or you can use the common endpoint
     // 'https://login.microsoftonline.com/common/.well-known/openid-configuration'
     // To use the common endpoint, you have to either set `validateIssuer` to false, or provide the `issuer` value.
-    identityMetadata: `https://login.microsoftonline.com/${process.env.AZURE_AD_TENANT_GUID}/.well-known/openid-configuration`,
+    identityMetadata: `https://login.microsoftonline.com/${
+      process.env.AZURE_AD_TENANT_GUID
+    }/.well-known/openid-configuration`,
 
     // Required, the client ID of your app in AAD
     clientID: process.env.AZURE_AD_CLIENT_ID,
@@ -54,8 +92,8 @@ exports.creds = {
     // rollover purpose. We always use the first set of key/iv pair to encrypt cookie, but we will try every set of
     // key/iv pair to decrypt cookie. Key can be any string of length 32, and iv can be any string of length 12.
     cookieEncryptionKeys: [
-        {'key': '12345678901234567890123456789012', 'iv': '123456789012'},
-        {'key': 'abcdefghijklmnopqrstuvwxyzabcdef', 'iv': 'abcdefghijkl'}
+      { key: '12345678901234567890123456789012', iv: '123456789012' },
+      { key: 'abcdefghijklmnopqrstuvwxyzabcdef', iv: 'abcdefghijkl' }
     ],
 
     // Optional. The additional scope you want besides 'openid', for example: ['email', 'profile'].
@@ -71,25 +109,20 @@ exports.creds = {
     nonceMaxAmount: 5,
 
     // Optional. The clock skew allowed in token validation, the default value is 300 seconds.
-    clockSkew: null,
+    clockSkew: null
+  },
+
+  resourceURL: 'https://graph.windows.net',
+  // The url you need to go to destroy the session with AAD
+  destroySessionUrl:
+    'https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=http://localhost:3000',
+  // If you want to use the mongoDB session store for session middleware; otherwise we will use the default
+  // session store provided by express-session.
+  // Note that the default session store is designed for development purpose only.
+  useMongoDBSessionStore: false,
+  // If you want to use mongoDB, provide the uri here for the database.
+  databaseUri: envVars.DATABASE_MONGO_URI,
+  // How long you want to keep session in mongoDB.
+
+  mongoDBSessionMaxAge: 24 * 60 * 60 // 1 day (unit is second)
 };
-
-// Optional.
-// If you want to get access_token for a specific resource, you can provide the resource here; otherwise,
-// set the value to null.
-// Note that in order to get access_token, the responseType must be 'code', 'code id_token' or 'id_token code'.
-exports.resourceURL = 'https://graph.windows.net';
-
-// The url you need to go to destroy the session with AAD
-exports.destroySessionUrl = 'https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri=http://localhost:3000';
-
-// If you want to use the mongoDB session store for session middleware; otherwise we will use the default
-// session store provided by express-session.
-// Note that the default session store is designed for development purpose only.
-exports.useMongoDBSessionStore = false;
-
-// If you want to use mongoDB, provide the uri here for the database.
-exports.databaseUri = process.env.DATABASE_MONGO_URI;
-
-// How long you want to keep session in mongoDB.
-exports.mongoDBSessionMaxAge = 24 * 60 * 60;  // 1 day (unit is second)
